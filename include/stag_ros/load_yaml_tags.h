40186e4ee246561dcfaffbac6b7325b7288c698c
Brennan Cain <brenn.cain@gmail.com>
2020-10-20 15:12:44 -0400

    beginning move, invalid return from parameter server

---
+++ include/stag_ros/load_yaml_tags.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+#include "structures.hpp"
+#include "ros/ros.h"
+
+
+namespace stag_ros {
+inline void loadTags(const XmlRpc::XmlRpcValue &xml, std::vector<Tag> &tags) {
+ // std::cout << xml.size() << std::endl;
+}
+inline void loadBundles(XmlRpc::XmlRpcValue xml, std::vector<Bundle> &bundles) {
+
+}
+
+}
