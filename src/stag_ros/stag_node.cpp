2d878279c3cd67bf5181e4b3a7e029d045bcb6d8
MikeK4y <mike.kalaitzakis@gmail.com>
2020-12-07 17:05:57 -0500

    Solved pose overwrite for multiple markers

--- src/stag_ros/stag_node.cpp
+++ src/stag_ros/stag_node.cpp
@@ -90,8 +90,7 @@ void StagNode::loadParameters() {
   nh_lcl.param("publish_tf", publish_tf, false);
   nh_lcl.param("tag_tf_prefix", tag_tf_prefix, std::string("STag_"));
 
-  loadTagsBundles(nh_lcl,"tags","bundles",tags,bundles);
-
+  loadTagsBundles(nh_lcl, "tags", "bundles", tags, bundles);
 }
 
 bool StagNode::getTagIndex(const int id, int &tag_index) {
@@ -145,8 +144,8 @@ void StagNode::imageCallback(const sensor_msgs::ImageConstPtr &msg) {
     // For each marker in the list
     if (markers.size() > 0) {
       // Create markers msg
-      std::vector<cv::Mat> tag_pose(tags.size(), cv::Mat(3, 4, CV_64F));
-      std::vector<cv::Mat> bundle_pose(bundles.size(), cv::Mat(3, 4, CV_64F));
+      std::vector<cv::Mat> tag_pose(tags.size(), cv::Mat::zeros(3, 4, CV_64F));
+      std::vector<cv::Mat> bundle_pose(bundles.size(), cv::Mat::zeros(3, 4, CV_64F));
       std::vector<std::vector<cv::Point2d>> bundle_image(bundles.size());
       std::vector<std::vector<cv::Point3d>> bundle_world(bundles.size());
 
@@ -167,8 +166,10 @@ void StagNode::imageCallback(const sensor_msgs::ImageConstPtr &msg) {
             tag_world[ci + 1] = tags[tag_index].corners[ci];
           }
 
-          Common::solvePnpSingle(tag_image, tag_world, tag_pose[tag_index],
+          cv::Mat marker_pose = cv::Mat::zeros(3, 4, CV_64F);
+          Common::solvePnpSingle(tag_image, tag_world, marker_pose,
                                  cameraMatrix, distortionMat);
+          tag_pose[tag_index] = marker_pose;
 
         } else if (getBundleIndex(markers[i].id, bundle_index, tag_index)) {
           bundle_image[bundle_index].push_back(markers[i].center);
@@ -184,9 +185,12 @@ void StagNode::imageCallback(const sensor_msgs::ImageConstPtr &msg) {
       }
 
       for (size_t bi = 0; bi < bundles.size(); ++bi) {
-        if (bundle_image[bi].size() > 0)
-          Common::solvePnpBundle(bundle_image[bi], bundle_world[bi],
-                                 bundle_pose[bi], cameraMatrix, distortionMat);
+        if (bundle_image[bi].size() > 0) {
+          cv::Mat b_pose = cv::Mat::zeros(3, 4, CV_64F);
+          Common::solvePnpBundle(bundle_image[bi], bundle_world[bi], b_pose,
+                                 cameraMatrix, distortionMat);
+          bundle_pose[bi] = b_pose;
+        }
       }
 
       for (size_t bi = 0; bi < bundles.size(); ++bi) {
