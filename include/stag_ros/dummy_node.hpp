ac6cfbcdb2f67528dc6f39a725f95a2275f94852
Baptiste Busch <baptiste.busch@gmail.com>
2022-06-21 15:11:34 +0200

    fix: add wip node conversion

---
+++ include/stag_ros/dummy_node.hpp
@@ -0,0 +1,49 @@
+#include <memory>
+
+// ROS includes
+#include <rclcpp/rclcpp.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <camera_info_manager/camera_info_manager.hpp>
+#include <sensor_msgs/image_encodings.hpp>
+
+
+
+// Stag includes
+#include "stag/Stag.h"
+#include "stag_ros/structures.hpp"
+#include "stag_ros/load_yaml_tags.hpp"
+
+namespace stag_ros {
+class DummyNode : public rclcpp::Node {
+ public:
+  explicit DummyNode();
+  ~DummyNode();
+
+ private:
+  void loadParameters();
+
+  // STag handle
+  Stag *stag;
+  int stag_library;
+  int error_correction;
+
+  // Data
+  cv::Mat cameraMatrix;
+  cv::Mat distortionMat;
+  cv::Mat rectificationMat;
+  cv::Mat projectionMat;
+  bool got_camera_info;
+  bool debug_images;
+  bool publish_tf;
+  bool is_compressed;
+  std::string image_topic;
+  std::string camera_info_topic;
+  std::string tag_tf_prefix;
+
+  // Tag and bundle info
+  std::string bundle_config_filepath;
+  std::vector<Bundle> bundles;
+  std::vector<Tag> tags;
+};
+}  // namespace stag_ros
---
+++ src/stag_ros/dummy_node.cpp
@@ -0,0 +1,37 @@
+#include "stag_ros/dummy_node.hpp"
+
+namespace stag_ros {
+DummyNode::DummyNode() : Node("dummy_node") {
+  this->declare_parameter<int>("libraryHD", 15);
+  this->declare_parameter<int>("errorCorrection", 7);
+  this->declare_parameter<std::string>("raw_image_topic", "image_raw");
+  this->declare_parameter<std::string>("camera_info_topic", "camera_info");
+  this->declare_parameter<bool>("is_compressed", false);
+  this->declare_parameter<bool>("show_markers", false);
+  this->declare_parameter<bool>("publish_tf", false);
+  this->declare_parameter<std::string>("tag_tf_prefix", "STag_");
+  this->declare_parameter<std::string>("tags", "");
+  this->declare_parameter<std::string>("bundles", "");
+}
+
+DummyNode::~DummyNode() { delete stag; }
+
+void DummyNode::loadParameters() {
+  this->get_parameter("libraryHD", this->stag_library);
+  this->get_parameter("errorCorrection", this->error_correction);
+  this->get_parameter("raw_image_topic", this->image_topic);
+  this->get_parameter("camera_info_topic", this->camera_info_topic);
+  this->get_parameter("is_compressed", this->is_compressed);
+  this->get_parameter("show_markers", this->debug_images);
+  this->get_parameter("publish_tf", this->publish_tf);
+  this->get_parameter("tag_tf_prefix", this->tag_tf_prefix);
+  loadTagsBundles(this->shared_from_this(), "tags", "bundles", this->tags, this->bundles);
+}
+}  // namespace stag_ros
+
+int main(int argc, char* argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<stag_ros::DummyNode>());
+  rclcpp::shutdown();
+  return 0;
+}
